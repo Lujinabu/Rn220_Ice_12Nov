88d5b2f50bcb0152a4df5257e965040ddfcf6a8c
Lujinabu <ms23570@bristol.ac.uk>
2024-11-13 12:34:18 +0000

    Initial commit

---
+++ main.cc
@@ -0,0 +1,56 @@
+#include "G4RunManagerFactory.hh"
+#include "G4UImanager.hh"
+#include "G4VisExecutive.hh"
+#include "G4UImanager.hh"
+#include "G4VisExecutive.hh"
+#include "G4UIExecutive.hh"
+#include "DetectorConstruction.hh"
+#include "PrimaryGeneratorAction.hh"
+#include "PhysicsList.hh"
+#include "RunAction.hh"
+#include "ActionInitialization.hh"
+int main(int argc, char** argv)
+{
+      G4UIExecutive* ui = nullptr;
+  if ( argc == 1 ) { ui = new G4UIExecutive(argc, argv); }
+    std::unique_ptr<G4RunManager> runManager(G4RunManagerFactory::CreateRunManager());
+    runManager->SetNumberOfThreads(1); // by default
+    
+    runManager->SetUserInitialization(new MyDetectorConstruction());
+    runManager->SetUserInitialization(new MyPhysicsList());
+    runManager->SetUserInitialization(new MyActionInitialization());
+     
+// Initialize visualization
+  //
+  G4VisManager* visManager = new G4VisExecutive;
+  // G4VisExecutive can take a verbosity argument - see /vis/verbose guidance.
+  // G4VisManager* visManager = new G4VisExecutive("Quiet");
+  visManager->Initialize();
+
+  // Get the pointer to the User Interface manager
+  G4UImanager* UImanager = G4UImanager::GetUIpointer();
+
+  // Process macro or start UI session
+  //
+  if ( ! ui ) {
+    // batch mode
+    G4String command = "/control/execute ";
+    G4String fileName = argv[1];
+    UImanager->ApplyCommand(command+fileName);
+  }
+  else {
+    // interactive mode
+    UImanager->ApplyCommand("/control/execute pos.mac");
+    UImanager->ApplyCommand("/control/execute vis.mac");
+    ui->SessionStart();
+    delete ui;
+  }
+
+  
+
+  // delete visManager;
+
+
+    return 0;
+}
+
